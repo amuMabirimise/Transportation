87f2d1e576dc7fdb96e09641074db06a5e010597
amuMabirimise <awmabirimise@gmail.com>
2024-07-26 19:47:39 +0200

    mobile-css

--- js/script.js
+++ js/script.js
@@ -35,6 +35,7 @@ function handleSubmit() {
     }
 }
 
+//navigate bar
 let menu = document.querySelector('#menu-bars');
 let navbar = document.querySelector('.navbar');
 
@@ -75,27 +76,3 @@ document.querySelector('#search-icon').onclick = () => {
 document.querySelector('#close').onclick = () => {
     document.querySelector('#search-form').classList.remove('active');
 }
-
-var swiper = new Swiper('.swiper-container', {
-  spaceBetween: 30,
-  centeredSlides: true,
-  autoplay: {
-      delay: 7500,
-      disableOnInteraction: false,
-  },
-  pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-  },
-  loop: true,
-});
-
-function loader(){
-    document.querySelector('.loader-container').classList.add('fade-out');
-}
-
-function fadeOut(){
-    setInterval(loader, 3000);
-}
-
-window.onload = fadeOut;
